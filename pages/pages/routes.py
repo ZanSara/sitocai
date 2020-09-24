b8748da818204cd1407b940392ba69a58c2283ca
ZanSara <sarazanzo94@gmail.com>
2020-09-24 22:44:16 +0200

    Review whole layout and add some content to homepage and home rifugio

--- pages/pages/routes.py
+++ pages/pages/routes.py
@@ -11,49 +11,49 @@ from flask_login import current_user, login_user, logout_user, login_required
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', selected="home")
 
 
 @app.route('/rifugio')
 @app.route('/rifugio/rifugio.html')
 def rifugio():
-    return render_template('rifugio.html')
+    return render_template('rifugio.html', selected="rifugio")
 
 @app.route('/rifugio/percorsi')
 @app.route('/rifugio/rifugio-percorsi/rifugio-percorsi.html')
 def rifugio_percorsi():
-    return render_template('rifugio-percorsi.html')
+    return render_template('rifugio-percorsi.html', selected="rifugio_percorsi")
 
-@app.route('/rifugio/webcams')
+@app.route('/rifugio/webcam')
 @app.route('/rifugio/webcam/webcam.html')
 def rifugio_webcams():
-    return render_template('rifugio-webcams.html')
+    return render_template('rifugio-webcams.html', selected="rifugio_webcam")
 
 @app.route('/rifugio/storia')
 @app.route('/rifugio/rifugio-storia/rifugio-storia.html')
 def rifugio_storia():
-    return render_template('rifugio-storia.html')
+    return render_template('rifugio-storia.html', selected="rifugio_storia")
 
 @app.route('/rifugio/foto')
 @app.route('/rifugio/rifugio-lavori/rifugio-lavori.html')
 def rifugio_foto():
-    return render_template('rifugio-foto.html')
+    return render_template('rifugio-foto.html', selected="rifugio_lavori")
 
 
 @app.route('/sezione')
 @app.route('/sezione/sezione.html') # Backward compatibility
 def sezione():
-    return render_template('sezione.html')
+    return render_template('sezione.html', selected="sezione")
 
 @app.route('/programmi')
 @app.route('/programmi/programmi.html') # Backward compatibility
 def programmi():
-    return render_template('programmi.html')
+    return render_template('programmi.html', selected="sezione_programmi")
 
 @app.route('/bacheca')
 @app.route('/bacheca/bacheca.html') # Backward compatibility
 def bacheca():
-    return render_template('bacheca.html')
+    return render_template('bacheca.html', selected="sezione_bacheca")
 
 
 
