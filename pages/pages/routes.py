7cc32722b081843fa5af6857400e6e4034714c85
ZanSara <sarazanzo94@gmail.com>
2020-09-26 23:57:35 +0200

    Add english version and basically complete the static pages. Missing Webcam and meteo

--- pages/pages/routes.py
+++ pages/pages/routes.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 
 from pages import app
 from pages.models import Utente
@@ -12,24 +12,39 @@ import datetime
 from urllib.request import urlopen
 
 
+def double_render_template(url, **kwargs):
+    if request.url.endswith('/en'):
+        return render_template('en/'+url, url=request.url.replace('/en', ''), **kwargs)
+
+    if request.url.endswith("_eng.html"):
+        return render_template('en/'+url, url=request.url.replace('"_eng.html', '.html'), **kwargs)
+
+    else:
+        return render_template(url, url=request.url+"/en", **kwargs)
+
 @app.route('/')
+@app.route('/en')
 @app.route('/index.html')
 def index():
-    return render_template('index.html', selected="home")
-
+    return double_render_template('index.html', selected="home")
 
 @app.route('/rifugio')
 @app.route('/rifugio/rifugio.html')
+@app.route('/rifugio/en')
+@app.route('/rifugio/rifugio_eng.html')
 def rifugio():
-    return render_template('rifugio.html', selected="rifugio")
+    return double_render_template('rifugio.html', selected="rifugio")
 
 @app.route('/rifugio/sentieri')
 @app.route('/rifugio/rifugio-percorsi/rifugio-percorsi.html')
+@app.route('/rifugio/sentieri/en')
+@app.route('/rifugio/rifugio-percorsi/rifugio-percorsi_eng.html')
 def rifugio_sentieri():
-    return render_template('rifugio-sentieri.html', selected="rifugio_sentieri")
+    return double_render_template('rifugio-sentieri.html', selected="rifugio_sentieri")
 
 @app.route('/rifugio/webcam')
 @app.route('/rifugio/webcam/webcam.html')
+@app.route('/rifugio/webcam/en')
 def rifugio_webcams():
     # Leggi l'archivio
     archivio = urlopen("http://www.caisovico.it/wm/wc2/meteo/dati.csv")
@@ -52,33 +67,54 @@ def rifugio_webcams():
         tabella["Status"] = "ONLINE"
     else:
         tabella["Status"] = "OFFLINE"
-    return render_template('rifugio-webcam.html', selected="rifugio_webcam", dati_meteo=tabella)
+    # Render
+    return double_render_template('rifugio-webcam.html', selected="rifugio_webcam", dati_meteo=tabella)
 
 @app.route('/rifugio/bivacco')
 @app.route('/rifugio/rifugio-chiuso/rifugio-chiuso.html')
+@app.route('/rifugio/bivacco/en')
+@app.route('/rifugio/rifugio-chiuso/rifugio-chiuso_eng.html')
 def rifugio_bivacco():
-    return render_template('rifugio-bivacco.html', selected="rifugio_bivacco")
+    return double_render_template('rifugio-bivacco.html', selected="rifugio_bivacco")
 
 @app.route('/rifugio/storia')
 @app.route('/rifugio/rifugio-storia/rifugio-storia.html')
+@app.route('/rifugio/storia/en')
+@app.route('/rifugio/rifugio-storia/rifugio-storia_eng.html')
 def rifugio_storia():
-    return render_template('rifugio-storia.html', selected="rifugio_storia")
+    return double_render_template('rifugio-storia.html', selected="rifugio_storia")
 
 
 @app.route('/sezione')
-@app.route('/sezione/sezione.html') # Backward compatibility
+@app.route('/sezione/sezione.html')
+@app.route('/sezione/en')
+@app.route('/sezione/sezione_eng.html')
 def sezione():
-    return render_template('sezione.html', selected="sezione")
+    return double_render_template('sezione.html', selected="sezione")
 
-@app.route('/programmi')
-@app.route('/programmi/programmi.html') # Backward compatibility
-def programmi():
-    return render_template('programmi.html', selected="sezione_programmi")
+@app.route('/sezione/quote')
+@app.route('/sezione/quote/en')
+@app.route('/sezione/sezione-quote.html')
+@app.route('/sezione/sezione-quote_eng.html')
+def sezione_quote():
+    return double_render_template('sezione-quote.html', selected="sezione_quote")
 
-@app.route('/bacheca')
-@app.route('/bacheca/bacheca.html') # Backward compatibility
-def bacheca():
-    return render_template('bacheca.html', selected="sezione_bacheca")
+@app.route('/sezione/storia')
+@app.route('/sezione/sezione-storia.html')
+@app.route('/sezione/storia/en')
+@app.route('/sezione/sezione-storia_eng.html')
+def sezione_storia():
+    return double_render_template('sezione-storia.html', selected="sezione_storia")
+
+@app.route('/sezione/bacheca')
+@app.route('/bacheca/bacheca.html')
+def sezione_bacheca():
+    return render_template('sezione-bacheca.html', selected="sezione_bacheca")
+
+@app.route('/sezione/programmi')
+@app.route('/programmi/programmi.html')
+def sezione_programmi():
+    return render_template('sezione-programmi.html', selected="sezione_programmi")
 
 
 
@@ -111,20 +147,35 @@ def logout():
 
 @app.errorhandler(401) 
 def unauthorized(e):
-  return render_template("401.html") 
+    if '/en/' in request.url:
+        return render_template("en/401.html") 
+    else:
+        return render_template("401.html") 
 
 @app.errorhandler(403) 
 def forbidden(e):
-  return render_template("403.html") 
+    if '/en/' in request.url:
+        return render_template("en/403.html") 
+    else:
+        return render_template("403.html") 
 
 @app.errorhandler(404) 
 def not_found(e):
-  return render_template("404.html") 
+    if '/en/' in request.url:
+        return render_template("en/404.html") 
+    else:
+        return render_template("404.html") 
 
 @app.errorhandler(405) 
 def method_not_allowed(e):
-  return render_template("405.html")
+    if '/en/' in request.url:
+        return render_template("en/405.html") 
+    else:
+        return render_template("405.html") 
 
 @app.errorhandler(500) 
 def internal_error(e):
-  return render_template("500.html") 
+    if '/en/' in request.url:
+        return render_template("en/500.html") 
+    else:
+        return render_template("500.html") 
